764bbdad438c953bb965d0aef5dbe1bc352039b6
César Lautaro Orellana <cesarlorellana184@gmail.com>
2026-09-16 16:10:26 -0300

    tp3 funcion misterio

--- Tp3/ejercicio1.hs
+++ Tp3/ejercicio1.hs
@@ -1,4 +1,10 @@
-misterio1 x
-| x <= 0 = 0
-| x == 1 = 1
-| otherwise = 2*x + misterio1 (x - 1) – 1
+{-  misterio1 recibe un numero n y devuelve n*n -}
+misterio1 x 
+    | x <= 0 = 0
+    | x == 1 = 1
+    | otherwise = 2*x + misterio1 (x - 1) - 1
+
+{-  misterio2 recibe un numero n suma los caracteres del numero -}
+misterio2 0 = 0
+misterio2 n = (mod n 10) + misterio2 (div n 10)
+
